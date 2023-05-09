378e9e13c2c3d9eb3f8ebb752bcc9105303e841c
tom-leamon <leamon@outlook.com>
2023-05-09 18:46:38 -0500

    Testing out Docking layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,122 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import { Grid, AspectRatio, Empty, Box, Docking, LoadingSpinner } from '@avsync.live/formation'
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
 
-import {
-  Page, RichTextEditor, markdownToHTML
-} from '@avsync.live/formation'
-import { useEffect, useState } from 'react';
+// import '@annotationhub/react-golden-layout/dist/css/goldenlayout-base.css';
+// import '@annotationhub/react-golden-layout/dist/css/themes/goldenlayout-dark-theme.css';
+// import { GoldenLayoutComponent } from '@annotationhub/react-golden-layout';
 
-const Home: NextPage = () => {
-  const [readme, setReadme] = useState('');
-
-  useEffect(() => {
-    async function fetchReadme() {
-      const response = await fetch('https://raw.githubusercontent.com/AVsync-LIVE/Formation/main/README.md');
-      const text = await response.text();
-      setReadme(text);
-    }
-    fetchReadme();
-  }, []);
-  
-  return (
-    <div>
-      <Head>
-        <title>Formation Next Example</title>
-        <meta name="description" content="This Next.js app was made using Formation" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Page>
+const PlaceholderContent = () => {
+    return <Box px={.5}>
+      <Grid maxWidth={10}>
         {
-          readme &&
-            <RichTextEditor
-              value={String(markdownToHTML(readme))}
-              readOnly
-            />
+          new Array(60).fill(0).map(i =>
+            <AspectRatio ratio={16/9}>
+              <Empty />
+            </AspectRatio>  
+          )
         }
-     
-      </Page>
-    </div>
-  )
+      </Grid>
+    </Box>
+  }
+
+interface Props {
+  
 }
 
-export default Home
+const test = ({ }: Props) => {
+  const [layoutManager, setLayoutManager] = useState({})
+
+  const [content, set_content] = useState(<S.LoadingContainer><LoadingSpinner /></S.LoadingContainer>)
+
+  useEffect(() => {
+    (async () => {
+      const { GoldenLayoutComponent } = await import('@annotationhub/react-golden-layout')
+      set_content(
+        <GoldenLayoutComponent
+          // (Required) Golden Layout Config. (See http://golden-layout.com/docs/Config.html)
+          config={{
+            settings: {
+              showPopoutIcon: false,
+              showMaximiseIcon: false,
+              showCloseIcon: false,
+              hasHeaders: true,
+            },
+            content: [
+              {
+                type: 'column',
+                content: [
+                  {
+                    type: 'row',
+                    content: [
+                      
+                      {
+                        type: 'column',
+                        content: [
+                          {
+                            component: () => <PlaceholderContent />,
+                            title: 'Playlist'
+                          }, 
+                        ]
+                      }
+                    ]
+                  },
+                  {
+                    type: 'row',
+                    content: [
+                      {
+                        component: () => <PlaceholderContent />,
+                        title: 'Visuals'
+                      },
+                      {
+                        component: () => <PlaceholderContent />,
+                        title: 'Effects'
+                      },
+                      {
+                        component: () => <PlaceholderContent />,
+                        title: 'Sources'
+                      },
+                    ]
+                  }
+                ]
+              }
+            ]
+          }}
+          autoresize={true}
+          debounceResize={100}
+          onLayoutReady={setLayoutManager}
+        />
+      )
+    })()
+  }, [])
+
+  return (
+    <S.DockingContainer>
+      {
+        content
+      }
+    </S.DockingContainer>
+  );
+}
+
+export default test
+
+
+const S = {
+  DockingContainer: styled.div`
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    position: relative;
+  `,
+  LoadingContainer: styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `
+}
